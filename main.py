c97f78663e70446f594705f57fc8d7259c625f81
zabolotnyitaras <103109396+zabolotnyitaras@users.noreply.github.com>
2022-04-06 14:32:26 +0300

    123456789

--- main.py
+++ main.py
@@ -5,6 +5,5 @@ string_sample2 = "first letteR is lowErcase"
 string_sample3 = " exstra whitespace string "
 german_sample = "der Flus"
 
-print(len(string_sample))
-print(string_sample[10:])
+print("")
 #[START:END:STEP]
